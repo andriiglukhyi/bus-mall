fccf68521fda8b3d54134b7af2a0e54538d62025
andriiglukhyi <andrigluxii@gmail.com>
2017-12-18 22:20:21 -0800

    85 persenr done

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 'use sctict'
 
 var pictures = [];
+var gCounter = 0;
 
  function Picture(url){
    this.url = url;
    this.name = this.url.split(".")[0];
-   this.timesShown=0;
+   this.timesShown = 0;
+   this.click = 0;
    this.shown=false;
    pictures.push(this);
   //  console.log(pictures);
  }
 
-var stuff=["bag.jpg","banana.jpg","bathroom.jpg","boots.jpg","breakfast.jpg"]
+var stuff=["bag.jpg","banana.jpg","bathroom.jpg","boots.jpg","breakfast.jpg","bubblegum.jpg","chair.jpg", "cthulhu.jpg", "dog-duck.jpg", "dragon.jpg", "pen.jpg", "pet-sweep.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "tauntaun.jpg", "unicorn.jpg", "usb.gif", "water-can.jpg", "wine-glass.jpg" ]
 
-for var (i=0; i<stuff.length; i++){
-  new Picture(stuff[i]);
+function newElement (){
+  for (var i=0; i < stuff.length; i++) {
+    new Picture(stuff[i]);
+    // console.log(pictures);
   }
+}
 
 var arr3img = function () {
    var counter = 0;
    var images = [];
    while (counter<6){
-    var ran = pictures[Math.floor(Math.random() * pictures.length)];
-    // console.log(ran);
+     var index = Math.floor(Math.random() * pictures.length);
+     var ran = pictures[index];
+     // console.log(ran);
     if (ran.shown === false){
       ran.timesShown++
       counter++
       ran.shown=true;
       images.push(ran);
+    }
+    for (var i = 0; i < pictures.length; i++) {
+      if (i !== index) {
+        pictures[i].shown = false;
+      }
     }
   }
   return images;
@@ -37,16 +48,38 @@ var arr3img = function () {
 function render(){
   var images = arr3img();
   for (var i = 0; i<images.length;i++){
-    document.getElementsById("pic1");
-    pic[i].innerHTML = "<img src=\"img/" + images[i]+"\">";
-    document.getElementsById("pic2");
-    pic[i].innerHTML = "<img src=\"img/" + images[i]+"\">";
-    document.getElementsById("pic3");
-    pic[i].innerHTML = "<img src=\"img/" + images[i]+"\">";
-}
+    document.getElementById('pic1').innerHTML = "<img src=\"img/" + images[i].url+"\">";
+    document.getElementById('pic2').innerHTML = "<img src=\"img/" + images[i].url+"\">";
+    document.getElementById('pic3').innerHTML = "<img src=\"img/" + images[i].url+"\">";
+  }
+  gCounter++
 }
 
 
 
-render();
-div.addEventListener('click', render);
+function newPictures(){
+  while (gCounter<0){
+  pic1.addEventListener('click', render);
+  pic2.addEventListener('click', render);
+  pic1.addEventListener('click', render);
+  render();
+
+  }
+
+}
+
+function begining(){
+  newElement();
+  document.getElementById('pic1').innerHTML = "<img src=\"img/" + pictures[0].url+"\">";
+  pic2=document.getElementById('pic2');
+  pic2.innerHTML = "<img src=\"img/" + pictures[1].url+"\">";
+  pic3=document.getElementById('pic3');
+  pic3.innerHTML = "<img src=\"img/" + pictures[2].url+"\">";
+
+}
+
+
+begining();
+
+
+newPictures();
