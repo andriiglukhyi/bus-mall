832aabac77a4d2bd1c90a2aa8c19c8ddf19a4cce
andriiglukhyi <andrigluxii@gmail.com>
2017-12-19 09:38:41 -0800

    fixed begining

--- app.js
+++ app.js
@@ -60,10 +60,10 @@ function render(){
 
 function begining(){
   newElement();
-  pic1 = document.getElementById('pic1').innerHTML = "<img src=\"img/" + pictures[0].url+"\" height=\"300\" width=\"300\">";
-  pic2 = document.getElementById('pic2').innerHTML = "<img src=\"img/" + pictures[1].url+"\" height=\"300\" width=\"300\">";
-  pic3 = document.getElementById('pic3').innerHTML = "<img src=\"img/" + pictures[2].url+"\" height=\"300\" width=\"300\">";
-
+  var images = arr3img();
+  document.getElementById('pic1').innerHTML = "<img src=\"img/" + images[0].url+"\" height=\"300\" width=\"300\">";
+  document.getElementById('pic2').innerHTML = "<img src=\"img/" + images[1].url+"\" height=\"300\" width=\"300\">";
+  document.getElementById('pic3').innerHTML = "<img src=\"img/" + images[2].url+"\" height=\"300\" width=\"300\">";
 }
 
 
