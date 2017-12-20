5b7633b693a83bf4aebc31df7e1b323a8731ced4
andriiglukhyi <andrigluxii@gmail.com>
2017-12-19 23:11:19 -0800

    not working

--- app.js
+++ app.js
@@ -22,27 +22,37 @@ function newElement (){
   }
 }
 
+
 var arr3img = function () {
    var counter = 0;
    var images = [];
    while (counter<3){
      var index = Math.floor(Math.random() * pictures.length);
      var ran = pictures[index];
-     // console.log(ran);
-    if (ran.shown === false){
-      ran.timesShown++
-      counter++
-      ran.shown=true;
-      images.push(ran);
-      console.log(images);
-    }
-    }
-    for (var i = 0; i < pictures.length; i++) {
-      if (i !== index) {
-        pictures[i].shown = false;
-      }
+     if (ran.shown === false || ran.showlast === false){
+       ran.timesShown++
+       counter++
+       ran.shown=true;
+       ran.showlast = true;
+       images.push(ran);
+//    for (var i = 0; i < pictures.length; i++) {
+//      if (i !== imag {
+//        pictures[i].shown = false;
+//        console.log(pictures);
+}
+}
+for (var i = 0; i < pictures.length; i++) {
+  if (i !== images[1] && images[2] && images[0]) {
+    pictures[i].shown = false;
+    console.log(pictures);
   }
-  return images;
+}
+// for (var i = 0; i < pictures.length; i++) {
+// if (i !== images[1] && images[2] && images[0]) {
+//   pictures[i].showlast = false;
+// }
+return images;
+// }
 }
 
 
@@ -51,7 +61,7 @@ function render(){
     document.getElementById('pic1').innerHTML = "<img src=\"img/" + images[0].url+"\">";
     document.getElementById('pic2').innerHTML = "<img src=\"img/" + images[1].url+"\">";
     document.getElementById('pic3').innerHTML = "<img src=\"img/" + images[2].url+"\">";
-    gCounter++
+    // gCounter++
   }
 
 
@@ -60,26 +70,56 @@ function render(){
 
 function begining(){
   newElement();
-  var images = arr3img();
-  document.getElementById('pic1').innerHTML = "<img src=\"img/" + images[0].url+"\">";
-  document.getElementById('pic2').innerHTML = "<img src=\"img/" + images[1].url+"\">";
-  document.getElementById('pic3').innerHTML = "<img src=\"img/" + images[2].url+"\">";
+  render();
+  gCounter++
+
 }
 
 
-pic1.addEventListener('click', render);
-pic2.addEventListener('click', render);
-pic3.addEventListener('click', render);
+pic1.addEventListener('click', newPictures);
+pic2.addEventListener('click', newPictures);
+pic3.addEventListener('click', newPictures);
 
 
 
-// https://stackoverflow.com/questions/25028853/addeventlistener-two-functions
+
+// function clickCounter(e){
+//   var qwe = e.pic.src.slice(49);
+//   for (var i=0; i<pictures.length; i++){
+//     if ( qwe ===pictures[i].url){
+//       pictures[i].click++
+//     }
+//   }
+// }
 
 function newPictures(){
-  while (gCounter<25){
+  clickCounter();
+  if (gCounter<25){
     render();
+    gCounter++
+} else{
+
+
+  var canvas = document.getElementById('chart');
+  var ctx = canvas.getContext('2d');
+  var chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels :
+    }
   }
 }
+}
+  //   // for (var i =0; i<3; i++){
+  //   //   images[i].click++
+  //  }
+  // } else{
+  //   var canvas = document.getElementById('chart');
+  //   var ctx = canvas.getContext('2d');
+
+
+
 
 begining();
-newPictures();
+
+// newPictures();
